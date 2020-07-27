83c5d4ac866e4a16b69d6b366cc19ad6043f5556
Shalini010 <55777979+Shalini010@users.noreply.github.com>
2020-07-27 10:42:40 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,13 @@
+text = open("sample.txt", "r") 
+d = dict() 
+for line in text: 
+	line = line.strip() 
+	line = line.lower() 
+	words = line.split(" ") 
+	for word in words: 
+		if word in d: 
+			d[word] = d[word] + 1
+		else: 
+			d[word] = 1
+for key in list(d.keys()): 
+	print(key, ":", d[key]) 
